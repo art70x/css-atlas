603da82c1ae131188b6c8f8002270c00d183eb09
xy.art70 <xy.art70@gmail.com>
2026-01-24 13:11:36 +0000

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import '@/assets/main.css'
 import Index from '@/pages/Index'
 import NotFound from '@/pages/NotFound'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
